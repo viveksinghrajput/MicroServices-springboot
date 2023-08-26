e866a3fa880e51bdb055bdc479e7cb40f7691b15
vivek kumar <viveksingh6262@rediffmail.com>
2023-08-26 10:53:01 +0530

    implement circuit breaker patter

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,3 @@
 
 z
 Juser-services/src/main/java/com/example/userservice/dto/DepartmentDto.java,5\c\5c9e0c1cd5ab85c5e5105d6cfdb395c1857e8867
-t
-Duser-services/src/main/java/com/example/userservice/dto/UserDto.java,7\0\704f45e0cb117161af9100c639d0e13f607cce67
--- user-services/src/main/java/com/example/userservice/controller/UserController.java
+++ user-services/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.DepartmentDto;
 import com.example.userservice.dto.ResponseDto;
+import com.example.userservice.dto.UserDto;
 import com.example.userservice.entity.User;
 import com.example.userservice.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static final String DEPARTMENT_SERVICES ="departmentService";
+
     @PostMapping("/saveUser")
     public ResponseEntity<User> saveUser(@RequestBody User user) {
         User savedUser = userService.saveUser(user);
@@ -22,8 +28,17 @@ public class UserController {
     }
 
     @GetMapping("{id}")
+    //@CircuitBreaker(name =DEPARTMENT_SERVICES,fallbackMethod = "departmentFallback")
+    @Retry(name = DEPARTMENT_SERVICES,fallbackMethod = "departmentFallback")
     public ResponseEntity<ResponseDto> getUser(@PathVariable("id") Long userId) {
         ResponseDto responseDto = userService.getUser(userId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    public ResponseEntity<ResponseDto> departmentFallback(Exception e){
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setUser(new UserDto(1001L,"jyoti","Singh","jyoti@gmail.com"));
+        responseDto.setDepartment(new DepartmentDto(5001L,"Electronics","Pune","EC"));
         return ResponseEntity.ok(responseDto);
     }
 }
